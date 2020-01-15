9159b68a89b58a5a2e2ea286932efc2110abe044
bruno prates <bprates10@gmail.com>
2020-01-15 16:39:17 -0300

    fix-controller and add routes

--- app/Controllers/Http/CompanyController.js
+++ app/Controllers/Http/CompanyController.js
@@ -1,6 +1,18 @@
-'use strict'
+"use strict";
+
+const Company = use("App/Models/Company");
 
 class CompanyController {
+  async create({ request }) {
+    const data = request.only([
+      "business_name",
+      "social_name",
+      "register",
+      "active"
+    ]);
+    const company = await Company.create(data);
+    return company;
+  }
 }
 
-module.exports = CompanyController
+module.exports = CompanyController;
--- app/Models/Company.js
+++ app/Models/Company.js
@@ -1,9 +1,7 @@
-'use strict'
+"use strict";
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
+const Model = use("Model");
 
-class Company extends Model {
-}
+class Company extends Model {}
 
-module.exports = Company
+module.exports = Company;
